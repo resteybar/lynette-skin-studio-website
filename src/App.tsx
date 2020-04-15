dc0395cfdebd9436575e1576069480dbd81fa9b4
resteybar <resteybar@csumb.edu>
2020-04-15 13:57:14 -0700

    Adjusted background image to fit entire screen in home page

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
   
   return (
     <div className='App'>
-      {/* <Home renderLinks={htmlLinks} /> */}
+      <Home renderLinks={htmlLinks} />
 
       <NavBarScrolling renderLinks={htmlLinks} />
       {/* <Product /> */}
